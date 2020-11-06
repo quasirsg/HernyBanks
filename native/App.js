a72df44c774bcbb5a8654a65f692e95b796073c0
Olivert Borges <olivert.ricardo@gmail.com>
2020-11-06 13:14:56 -0300

    Utilizando Navigation

--- native/App.js
+++ native/App.js
@@ -1,16 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {NavigationContainer} from '@react-navigation/native';//instalar
+import { createStackNavigator } from '@react-navigation/stack'//instalar
+import Estatistics from './screens/Estatistics';
+import Login from './screens/Login';
+import PosConsolidada from './screens/PosConsolidada';
+import RechargeScreen from './screens/RechargeScreen';
+import Register from './screens/Register';
+import SendMonyScreen from './screens/SendMonyScreen';
+import Transactions from './screens/Transactions';
+const Stack = createStackNavigator()//contiene la navegacion
+//stack.screen contiene la pantalla
+function MyStack(){
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Estatistics" component={Estatistics}/>
+      <Stack.Screen name="Login" component={Login}/>
+      <Stack.Screen name="PosConsolidada" component={PosConsolidada}/>
+      <Stack.Screen name="RechargeScreen" component={RechargeScreen}/>
+      <Stack.Screen name="Register" component={Register}/>
+      <Stack.Screen name="SendMonyScreen" component={SendMonyScreen}/>
+      <Stack.Screen name="Transactions" component={Transactions}/>
+    </Stack.Navigator>
+  )
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer style={styles.container}>
+      <MyStack/>
+    </NavigationContainer>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ native/screens/Transactions.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Transactions() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
