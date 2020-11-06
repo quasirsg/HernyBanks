e65ff8b277565db05c5520300e7e3ad1b7f7f57c
Olivert Borges <olivert.ricardo@gmail.com>
2020-11-06 12:53:08 -0300

    Instalado Navigator y las Vistas Funcionando

---
+++ native/screens/Estatistics.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function Estatistics() {
+    return (
+        <View>
+            <Text>jbfjkegf</Text>
+        </View>
+    )
+}
+
+export default Estatistics
---
+++ native/screens/SendMonyScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function SendMonyScreen() {
+    return (
+        <View>
+            <Text>jbfjkegf</Text>
+        </View>
+    )
+}
+
+export default SendMonyScreen
