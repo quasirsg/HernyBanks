e12501b7a1f97fbcc7223a28d5205cf63a7e6697
Romano Javier <alexis_b13@live.com.ar>
2020-11-07 23:42:30 -0300

    resuelto

--- native/screens/Transactions.js
+++ native/screens/Transactions.js
@@ -1,17 +1,6 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-<<<<<<< HEAD
-function Transactions() {
-    return (
-        <View>
-            <Text>jbfjkegf</Text>
-        </View>
-    )
-}
-
-export default Transactions
-=======
 export default function Transactions() {
     return (
         <View>
@@ -19,4 +8,3 @@ export default function Transactions() {
         </View>
     )
 }
->>>>>>> 9ac71ba3b43bbf6efc9c2e975909ba707b34e638
