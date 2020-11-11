01e55cdcfba667f02c370cf7644de0bc0e6d6838
Romano Javier <alexis_b13@live.com.ar>
2020-11-11 11:15:00 -0300

    Register al 90%, login recibiendo el token-incompleto,combineReducers

--- native/store/reducer/userReducer.js
+++ native/store/reducer/userReducer.js
@@ -2,19 +2,21 @@ import { CREATE_USER, LOGIN_USER } from "../constans/constans";
 
 const initialState = {
   users: [],
-  user: null
+  userDetail: [],
+  err: [],
+  message: "",
 };
 
-const Reducer = (state = initialState, action) => {
+const userReducers = (state = initialState, action) => {
   console.log(action);
   switch (action.type) {
     case CREATE_USER:
       return { ...state, users: state.users.concat(action.users) };
-    case LOGIN_USER:
-      return { user: action.users };
+    // case LOGIN_USER:
+    //   return { user: action.users };
     default:
       return state;
   }
 };
 
-export default Reducer;
+export default userReducers;
