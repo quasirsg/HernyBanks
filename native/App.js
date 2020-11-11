0131f75c314a4d90e551fe4aca4c9df80eb07c0b
Martin <martincatala14@gmail.com>
2020-11-11 11:51:02 -0300

    Midpoint before pull

--- native/App.js
+++ native/App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';//instalar
-import { createStackNavigator } from '@react-navigation/stack'//instalar
-import Register from './screens/Register'
+import { NavigationContainer } from '@react-navigation/native'; //instalar
+import { createStackNavigator } from '@react-navigation/stack'; //instalar
+import Register from './screens/Register';
 import { Provider } from 'react-redux';
-import  { st }  from './store/store';
+import { st } from './store/store';
 import Estatistics from './screens/Estatistics';
 import Login from './screens/Login';
 import PosConsolidada from './screens/PosConsolidada';
@@ -13,36 +13,36 @@ import RechargeScreen from './screens/RechargeScreen';
 import Welcome from './screens/welcome';
 import SendMonyScreen from './screens/SendMonyScreen';
 import Transactions from './screens/Transactions';
-const Stack = createStackNavigator()//contiene la navegacion
+const Stack = createStackNavigator(); //contiene la navegacion
 //stack.screen contiene la pantalla
-function MyStack(){
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Welcome" component={Welcome} options={{headerShown:false}}/>
-      <Stack.Screen name="Register" component={Register} />
-      <Stack.Screen name="Estatistics" component={Estatistics}/>
-      <Stack.Screen name="Login" component={Login}/>
-      <Stack.Screen name="PosConsolidada" component={PosConsolidada}/>
-      <Stack.Screen name="RechargeScreen" component={RechargeScreen}/>
-      <Stack.Screen name="SendMonyScreen" component={SendMonyScreen}/>
-      <Stack.Screen name="Transactions" component={Transactions}/> 
-    </Stack.Navigator>
-  )
+function MyStack() {
+	return (
+		<Stack.Navigator>
+			<Stack.Screen name='PosConsolidada' component={PosConsolidada} options={{ headerShown: false }} />
+			<Stack.Screen name='Welcome' component={Welcome} options={{ headerShown: false }} />
+			<Stack.Screen name='Register' component={Register} />
+			<Stack.Screen name='Estatistics' component={Estatistics} />
+			<Stack.Screen name='Login' component={Login} />
+			<Stack.Screen name='RechargeScreen' component={RechargeScreen} />
+			<Stack.Screen name='SendMonyScreen' component={SendMonyScreen} />
+			<Stack.Screen name='Transactions' component={Transactions} />
+		</Stack.Navigator>
+	);
 }
 export default function App() {
-  return (
-    <Provider store={st}>
-    <NavigationContainer style={styles.container}>
-      <MyStack/>
-    </NavigationContainer>
-    </Provider>
-  );
+	return (
+		<Provider store={st}>
+			<NavigationContainer style={styles.container}>
+				<MyStack />
+			</NavigationContainer>
+		</Provider>
+	);
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
