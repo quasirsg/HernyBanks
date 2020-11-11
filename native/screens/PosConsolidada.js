5dbe46bdf7fc0666c2b5ec4338adaa6c764e549c
Martin <martincatala14@gmail.com>
2020-11-11 13:58:55 -0300

    PosConsolidada

--- native/screens/PosConsolidada.js
+++ native/screens/PosConsolidada.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
 
 // Background Image
