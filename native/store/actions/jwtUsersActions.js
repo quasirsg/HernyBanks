b7a5bf5439a71fc2583236d6c176357805683577
Romano Javier <alexis_b13@live.com.ar>
2020-11-15 00:51:32 -0300

    Incompleto

--- native/store/actions/jwtUsersActions.js
+++ native/store/actions/jwtUsersActions.js
@@ -1,92 +1,104 @@
-import axios from 'axios';
-import jwt_decode from 'jwt-decode';
+import axios from "axios";
+import jwt_decode from "jwt-decode";
 
-import  {BACK_URL}  from '../../env';
-import  AsyncStorage  from 'react-native';
+import { BACK_URL } from "../../env";
+import AsyncStorage from "react-native";
 
 // const {URL} = BACK_URL
 
-
-import * as actionTypes from '../constans/constans';
+import * as actionTypes from "../constans/constans";
 // const env = require('../../env.js')
 
 // const localhost= env.localhost;
 
-
-
 //loguin  -> funciona loguin correcto e incorrecto.
-export const loguinUser = (email, password) => (dispatch) => {
-	try {
-		axios
-			.post(`${BACK_URL}/api/auth/login`, {
-				email: email,
-				password: password,
-			})
-			.then((res) => {
-				console.log(res);
-				const token = res.data;
-				var decoded = jwt_decode(token);
-				console.log(decoded.id);
-				// console.log('soy el token',token);
-				if (token) {
-					AsyncStorage.setItem('@token', token);
-					dispatch({
-						type: actionTypes.USER_LOGIN,
-					});
-					dispatch(getCurrentUser(token));
-					// Swal.fire({
-					//   position: "center",
-					//   icon: "success",
-					//   title: `¡Bienvenido!`,
-					//   showConfirmButton: false,
-					//   timer: 2000,
-					// });
-				}
-			});
-		// .catch((error) => {
-		//   Toast.fire({
-		//     icon: "error",
-		//     title: "Error: email o contraseña no válidos",
-		//   });
-		// });
-	} catch {
-		dispatch({
-			type: actionTypes.USER_LOGIN_ERROR,
-			message: 'Error de login',
-		});
-	}
+export const loguinUser = (email, password, onSuccess) => (dispatch) => {
+  axios
+    .post(`${BACK_URL}/api/auth/login`, {
+      email: email,
+      password: password,
+    })
+    .then((res) => {
+      console.log(res);
+      const token = res.data;
+      var decoded = jwt_decode(token);
+      console.log(decoded.id);
+      // console.log('soy el token',token);
+      if (token) {
+        AsyncStorage.setItem("@token", token);
+        dispatch({
+          type: actionTypes.USER_LOGIN,
+        });
+        dispatch(getCurrentUser(token));
+        onSuccess();
+      }
+    });
+
+  Toast.show({
+    type: "error",
+    position: "top",
+    text1: "Error al loguearse",
+    visibilityTime: 6000,
+    autoHide: true,
+    topOffset: 30,
+    bottomOffset: 40,
+  });
 };
 
 //obtener información del usuario logueado
 export const getCurrentUser = (token) => async (dispatch) => {
-	//Headers con Token
-	var decoded = jwt_decode(token);
-	console.log(typeof decoded.id);
-	axios
-		.get(`${BACK_URL}/api/users/by-id`, {
-			params: {
-				_id: decoded.id,
-			},
-		})
-		.then((res) => {
-			dispatch({
-				type: actionTypes.CURRENT_USER,
-				user: res.data,
-			});
-		});
+  //Headers con Token
+  var decoded = jwt_decode(token);
+  console.log(typeof decoded.id);
+  axios
+    .get(`${BACK_URL}/api/users/by-id`, {
+      params: {
+        _id: decoded.id,
+      },
+    })
+    .then((res) => {
+      dispatch({
+        type: actionTypes.CURRENT_USER,
+        user: res.data,
+      });
+      Toast.show({
+        type: "success",
+        position: "top",
+        text1: `Bienvenido ${res.data.name} `,
+        visibilityTime: 6000,
+        autoHide: true,
+        topOffset: 30,
+        bottomOffset: 40,
+      });
+      setTimeout(function () {
+        onSuccess();
+      }, 3000);
+    })
+    .catch((eror) => {
+      Toast.show({
+        type: "error",
+        position: "top",
+        text1: "Error",
+        text2: `${error.message}`,
+        visibilityTime: 6000,
+        autoHide: true,
+        topOffset: 30,
+        bottomOffset: 40,
+      });
+    });
 };
 
 export const verifySession = () => (dispatch) => {
-	const { token } = AsyncStorage;
-	if (token) {
-		alert('usuario logeado');
-		dispatch(getCurrentUser(token));
-	} else {
-		dispatch({
-			type: actionTypes.NOT_CURRENT_USER,
-			message: 'No hay un usuario logueado.',
-		});
-	}
+  const { token } = AsyncStorage;
+  if (token) {
+    alert("usuario logeado");
+    dispatch(getCurrentUser(token));
+  } else {
+    dispatch({
+      type: actionTypes.NOT_CURRENT_USER,
+      message: "No hay un usuario logueado.",
+    });
+  }
 };
 
 //logout
@@ -103,16 +115,24 @@ export const logoutUser = (path) => (dispatch) => {
   //   cancelButtonText: "Cancelar",
   //   confirmButtonText: "Cerrar sesión",
   // }).then((res) => {
-    // if (res.isConfirmed) {
-      alert("¡Has cerrado sesión!", `Hasta la proxima`, "info");
-      dispatch({
-        type: actionTypes.LOGOUT_USER,
-      });
-      dispatch({
-        type: actionTypes.DELETE_ALL_CART,
-      });
-      AsyncStorage.removeItem("@token");
-    // }
+  // if (res.isConfirmed) {
+  Toast.show({
+    type: "info",
+    position: "top",
+    text1: "Cerraste sesión",
+    visibilityTime: 6000,
+    autoHide: true,
+    topOffset: 30,
+    bottomOffset: 40,
+  });
+  dispatch({
+    type: actionTypes.LOGOUT_USER,
+  });
+  dispatch({
+    type: actionTypes.DELETE_ALL_CART,
+  });
+  AsyncStorage.removeItem("@token");
+  // }
   // });
 };
 
